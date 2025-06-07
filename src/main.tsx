ae9f7409a1f99147b5a81898bc4a8e0913304745
Your Name <YOUR_EMAIL@users.noreply.github.com>
2025-06-07 22:58:40 +0200

    Fix: Tailwind Build-Fehler durch korrektes PostCSS-Plugin behoben

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css' // ← wichtig
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // ← wichtig!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
